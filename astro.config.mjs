cec008a67048e604d512e38499fe7e8887f68b4b
Tyler Nickerson <nickersoft@gmail.com>
2024-08-10 17:41:14 -0700

    change to vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +14,5 @@ export default defineConfig({
     }),
   ],
   output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
