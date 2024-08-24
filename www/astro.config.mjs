359c8ce724dcf11c520ecad31cf92458277a62cd
Tyler Nickerson <nickersoft@gmail.com>
2024-08-24 12:10:09 -0700

    wip

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -9,16 +9,21 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), starlight({
-    title: "ATMX",
-    customCss: ["./src/styles/global.css"]
-  }), mdx()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    starlight({
+      title: "ATMX",
+      // customCss: ["./src/styles/global.css"]
+    }),
+    mdx(),
+  ],
   site: "https://atmx.dev",
   output: "hybrid",
   adapter: vercel({
-    webAnalytics: true
-  })
+    webAnalytics: true,
+  }),
   // node({ mode: "standalone" }),
 });
