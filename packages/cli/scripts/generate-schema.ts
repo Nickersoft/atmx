3dc1d2bad963c03f0cdf720fe9d0931a2e52736d
Tyler Nickerson <nickersoft@gmail.com>
2024-09-08 12:18:05 -0700

    wip

--- packages/cli/scripts/generate-schema.ts
+++
@@ -1,8 +0,0 @@
-import { configSchema } from "@/config.ts";
-import { writeFile } from "node:fs/promises";
-
-import { zodToJsonSchema } from "zod-to-json-schema";
-
-export const jsonSchema = zodToJsonSchema(configSchema);
-
-await writeFile("schema.json", JSON.stringify(jsonSchema, null, 2));
--- packages/cli/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-  test: { watch: false },
-  plugins: [tsconfigPaths()],
-});
